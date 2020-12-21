9543a8555880c04f21da5407295b76166ae06607
SungmiKim <62002100+SungmiKim@users.noreply.github.com>
2020-12-21 22:47:52 +0900

    2020-12-21
    2020-12-21

--- js/main.js
+++ js/main.js
@@ -11,6 +11,14 @@
       scrollHeight: 0,
       objs: {
         container: document.querySelector("#scroll-section-0"),
+        messageA: document.querySelector("#scroll-section-0 .main-message.a"),
+        messageB: document.querySelector("#scroll-section-0 .main-message.b"),
+        messageC: document.querySelector("#scroll-section-0 .main-message.c"),
+        messageD: document.querySelector("#scroll-section-0 .main-message.d"),
+      },
+
+      values: {
+        messageA_opacity: [0, 1],
       },
     },
     {
@@ -64,6 +72,44 @@
     document.body.setAttribute("id", `show-scene-${currentScene}`);
   }
 
+  function calcValues(values, currentYOffset) {
+    let rv;
+    // 현재 씬(스크롤섹션)에서 스크롤된 범위를 비율로 구하기
+    let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
+
+    rv = scrollRatio * (values[1] - values[0]) + values[0];
+    console.log(rv);
+    return rv;
+  }
+
+  function palyAnimation() {
+    const objs = sceneInfo[currentScene].objs;
+    const values = sceneInfo[currentScene].values;
+    const currentYOffset = yOffset - prevScrollHeight;
+
+    switch (currentScene) {
+      case 0:
+        let messageA_opacity_in = calcValues(
+          values.messageA_opacity,
+          currentYOffset
+        );
+        objs.messageA.style.opacity = messageA_opacity_in;
+        break;
+
+      case 1:
+        // console.log("1 play");
+        break;
+
+      case 2:
+        // console.log("2 play");
+        break;
+
+      case 3:
+        // console.log("3 play");
+        break;
+    }
+  }
+
   function scrollLoop() {
     prevScrollHeight = 0;
     for (let i = 0; i < currentScene; i++) {
@@ -80,6 +126,8 @@
       currentScene--;
       document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
+
+    palyAnimation();
   }
 
   window.addEventListener("scroll", () => {
